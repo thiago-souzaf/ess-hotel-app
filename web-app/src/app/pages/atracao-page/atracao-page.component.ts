3e6f53d6969ce673d98d8e2ef8d03cbe39e13a70
Guilherme Melo <gacm@cin.ufpe.br>
2023-04-09 12:18:48 -0300

    remove hardcoded values from the atracao-page

--- web-app/src/app/pages/atracao-page/atracao-page.component.ts
+++ web-app/src/app/pages/atracao-page/atracao-page.component.ts
@@ -1,18 +1,41 @@
 import { Component } from '@angular/core';
+import { Atracao } from './atracao';
 
 @Component({
   selector: 'app-atracao-page',
   templateUrl: './atracao-page.component.html',
-  styleUrls: ['./atracao-page.component.scss']
+  styleUrls: ['./atracao-page.component.scss'],
 })
 export class AtracaoPageComponent {
+  currentAtracao: Atracao = {
+    id: '11a24b90',
+    name: 'Passeio de 1 dia com almoço em Angra dos Reis',
+    images: [
+      { url: 'assets/images/atracao-1/1.jpg', alt: 'photo-1' },
+      { url: 'assets/images/atracao-1/2.jpg', alt: 'photo 2' },
+      { url: 'assets/images/atracao-1/3.jpg', alt: 'photo 3' },
+      { url: 'assets/images/atracao-1/4.jpg', alt: 'photo 4' },
+      { url: 'assets/images/atracao-1/5.jpg', alt: 'photo 5' },
+    ],
+    title:
+      'Um passeio para descobrir a costa e fazer um passeio de barco pelas ilhas locais',
+    descriptions: [
+      'Este passeio lhe dará a oportunidade de viajar para a cidade litorânea de Angra dos Reis e explorar a baía.',
+      'Você partirá do Rio de Janeiro e fará um passeio de carro pela Costa Verde. Ao chegar em Angra dos Reis, você irá até o porto, onde poderá embarcar em um cruzeiro de barco de 4 horas ao redor da Ilha Grande. Você vai parar em 3 locais deslumbrantes para nadar e fazer mergulho com snorkel nas águas cristalinas e descobrir seus habitantes marinhos.',
+      'Durante o cruzeiro, você também terá a oportunidade de conhecer a história da ilha e da baía. O capitão ancorará em uma pequena ilha, onde você poderá almoçar em um restaurante acolhedor. No final do passeio, você voltará ao porto de Angra dos Reis e seguirá para o Rio de Janeiro.',
+    ],
+    location: 'Hotel Accomodare',
+    day: 'Segunda, 13 de Abril de 2023',
+    hour: 'Início: 07:00',
+  };
+
   isDrawbarVisible: boolean = false;
 
-  openDrawbar(): void{
+  openDrawbar(): void {
     this.isDrawbarVisible = true;
   }
 
-  closeDrawbar(): void{
+  closeDrawbar(): void {
     this.isDrawbarVisible = false;
   }
 }
